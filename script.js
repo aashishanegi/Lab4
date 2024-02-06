90bf41c855b4208e72327a387520aeb2de5b911b
aashishanegi <aashishanegi2303@gmail.com>
2024-02-06 14:14:20 +0530

    JS file added

---
+++ script.js
@@ -0,0 +1,7 @@
+function submitForm() {
+    
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    alert("Username: " + username + "\nPassword: " + password);
+}
